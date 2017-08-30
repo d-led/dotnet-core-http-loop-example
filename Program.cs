c8b8f65601503328ac4bd4b6614ab227359c6aa4
Dmitry Ledentsov <dmlled@yahoo.com>
2017-08-30 21:23:07 +0200

    project init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_core_http_loop_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
