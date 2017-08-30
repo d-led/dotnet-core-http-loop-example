456f5b52eb2b8cadf1debda89ff5362477abfa46
Dmitry Ledentsov <dmlled@yahoo.com>
2017-08-30 22:17:49 +0200

    loop complete

--- Program.cs
+++ Program.cs
@@ -1,17 +1,66 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Nancy.Hosting.Self;
+using Flurl;
+using Flurl.Http;
 
 namespace dotnet_core_http_loop_example
 {
+
+    public class Example : Nancy.NancyModule
+    {
+        public Example()
+        {
+            Get("/{id}", args => $"Hello World {args.id}!");
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
-        {
+        readonly string url = "http://localhost:1234";
+
+        void Client(string[] args) {
             int runs = 10;
             if (args.Length>0)
             int.TryParse(args[0], out runs);
 
-            for (int run =0; run<runs; run++) {
-                Console.WriteLine($"Hello World {run}!");
+            FlurlHttp.Configure(c => {
+                c.DefaultTimeout = TimeSpan.FromSeconds(30);
+                });
+
+            // create some requests and exit
+            Task.Run(() =>
+            {
+                for (int run =0; run<runs; run++) {
+                    System.Threading.Thread.Sleep(1000);
+                    var response = url
+                        .AppendPathSegment($"{run}")
+                        .GetStringAsync()
+                        .Result;
+                    Console.WriteLine(response);
+                }
+                System.Environment.Exit(0);
+                });
+        }
+
+        void Server() {
+            using (var host = new NancyHost(new Uri(url)))
+            {
+                host.Start();
+                Console.WriteLine($"Running on {url}");
+                Console.ReadLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try {
+                var program = new Program();
+                program.Client(args);
+                program.Server();
+            } catch (Exception e) {
+                Console.WriteLine($"ERROR: {e}");
+                System.Environment.Exit(1);
             }
         }
     }
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 dotnet core http loop
 =====================
 
+[![Build Status](https://travis-ci.org/d-led/dotnet-core-http-loop-example.svg?branch=master)](https://travis-ci.org/d-led/dotnet-core-http-loop-example)
+
 history
 -------
 
@@ -8,3 +10,24 @@ history
 - `cd dotnet-core-http-loop-example`
 - some hello-world code
 - `dotnet run -- 7`
+- `dotnet add package Nancy.Hosting.Self --version 2.0.0-clinteastwood`
+- `dotnet add package Nancy --version 2.0.0-clinteastwood`
+- `dotnet add package Flurl --version 2.4.0`
+- `dotnet add package Flurl.Http`
+- some NancyFX & Flurl code
+- `dotnet run -- 7`
+
+&darr;
+
+```
+dotnet-core-http-loop-example.csproj : warning NU1701: Package 'Nancy.Hosting.Self 2.0.0-clinteastwood' was restored using '.NETFramework,Version=v4.6.1' instead of the project target framework '.NETCoreApp,Version=v2.0'. This package may not be fully compatible with your project.
+dotnet-core-http-loop-example.csproj : warning NU1701: Package 'Nancy.Hosting.Self 2.0.0-clinteastwood' was restored using '.NETFramework,Version=v4.6.1' instead of the project target framework '.NETCoreApp,Version=v2.0'. This package may not be fully compatible with your project.
+Running on http://localhost:1234
+Hello World 0!
+Hello World 1!
+Hello World 2!
+Hello World 3!
+Hello World 4!
+Hello World 5!
+Hello World 6!
+```
