f7b886473c9436d060ebdbf00a2a2efe9313506f
Dmitry Ledentsov <dmlled@yahoo.com>
2017-09-06 01:33:33 +0200

    try installing a newer version

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -5,6 +5,12 @@ if ! [ -x "$(command -v docker)" ]; then
   exit 0
 fi
 
+# just for travis-ci
+if [ "TRAVIS" = "true" ]; then
+  sudo apt-get update
+  sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
+fi
+
 docker --version
 docker build -t dotnet-core-http-loop-example .
 docker run --rm dotnet-core-http-loop-example 8
