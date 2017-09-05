d655e68019a11e873d19d633e24d006c2626ffdc
Dmitry Ledentsov <dmlled@yahoo.com>
2017-09-06 01:44:55 +0200

    don't run docker on travis-ci, yet

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -5,15 +5,6 @@ if ! [ -x "$(command -v docker)" ]; then
   exit 0
 fi
 
-# just for travis-ci
-# https://docs.travis-ci.com/user/docker/
-if [ "TRAVIS" = "true" ]; then
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get -y install docker-ce
-fi
-
 docker --version
 docker build -t dotnet-core-http-loop-example .
 docker run --rm dotnet-core-http-loop-example 8
