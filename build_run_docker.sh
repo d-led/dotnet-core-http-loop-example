c5cb83c16e9b5971ef54bcefec31b4a4bfb78745
Dmitry Ledentsov <dmlled@yahoo.com>
2017-09-06 01:37:27 +0200

    yet another attempt

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -8,8 +8,10 @@ fi
 # just for travis-ci
 # https://docs.travis-ci.com/user/docker/
 if [ "TRAVIS" = "true" ]; then
-  sudo apt-get update
-  sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
 fi
 
 docker --version
